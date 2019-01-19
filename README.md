1151109551aaf5495f6c8a85577def9a07fc1a60
cell <cell@localhost.localdomain>
2019-01-19 13:45:34 -0500

    userSchema

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This is a simple chat app using React and websockets.
+
+npm i
+npm run server
+npm run client
---
+++ config/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    name: 'Jack',
+    lastName: 'London',
+    statusText: 'Available',
+    image: 'users/jack.png'
+  },
+  {
+    name: 'Sara',
+    lastName: 'Bennigton',
+    statusText: 'Available',
+    image: 'users/sara.png'
+  }
+]
